069a57f5bafba382de74305b69cab2b4102b8f1d
Andrei <ikar2008@hotmail.com>
2023-03-24 11:59:50 +0300

    Add working clock

--- Watch/Watch.cpp
+++ Watch/Watch.cpp
@@ -1,41 +1,28 @@
-﻿// FormC++App.cpp : Определяет точку входа для приложения.
+﻿// Watch.cpp : Определяет точку входа для приложения.
+//
 
-#define _CRT_SECURE_NO_WARNIGNS
-
+#define _CRT_SECURE_NO_WARNINGS
 #include "framework.h"
-#include "FormC++App.h"
+#include "Watch.h"
+
 
 #define MAX_LOADSTRING 100
-static int cx, cy;
-int width = 500;
-int height = 500;
-int x = 100;
-int y = 100;
-int Wx = 0;
-int Wy = 0;
-int WinX = 0;
-int WinY = 0;
-
-
+#define CLOCK_TIMER 1
 // Глобальные переменные:
 HINSTANCE hInst;                                // текущий экземпляр
 WCHAR szTitle[MAX_LOADSTRING];                  // Текст строки заголовка
 WCHAR szWindowClass[MAX_LOADSTRING];            // имя класса главного окна
+static time_t t;
+int nBeepTimerID;
+TCHAR _timebuf[MAX_LOADSTRING];
+TEXTMETRIC tm;
+int cxChar, cyChar;
 
-int l, r, m, ld, rd, md;
 // Отправить объявления функций, включенных в этот модуль кода:
 ATOM                MyRegisterClass(HINSTANCE hInstance);
 BOOL                InitInstance(HINSTANCE, int);
 LRESULT CALLBACK    WndProc(HWND, UINT, WPARAM, LPARAM);
 INT_PTR CALLBACK    About(HWND, UINT, WPARAM, LPARAM);
-
-RECT rect;
-POINT koord;
-
-void SetTitle(void)
-{
-    wsprintf(szTitle, _T("l=%d, r=%d, m=%d, ld=%d, rd=%d, md=%d"), l, r, m, ld, rd, md);
-}
 
 int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
                      _In_opt_ HINSTANCE hPrevInstance,
@@ -49,7 +36,7 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
 
     // Инициализация глобальных строк
     LoadStringW(hInstance, IDS_APP_TITLE, szTitle, MAX_LOADSTRING);
-    LoadStringW(hInstance, IDC_FORMCAPP, szWindowClass, MAX_LOADSTRING);
+    LoadStringW(hInstance, IDC_WATCH, szWindowClass, MAX_LOADSTRING);
     MyRegisterClass(hInstance);
 
     // Выполнить инициализацию приложения:
@@ -58,9 +45,11 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
         return FALSE;
     }
 
-    HACCEL hAccelTable = LoadAccelerators(hInstance, MAKEINTRESOURCE(IDC_FORMCAPP));
+    HACCEL hAccelTable = LoadAccelerators(hInstance, MAKEINTRESOURCE(IDC_WATCH));
 
     MSG msg;
+
+   
 
     // Цикл основного сообщения:
     while (GetMessage(&msg, nullptr, 0, 0))
@@ -74,9 +63,20 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
 
     return (int) msg.wParam;
 }
+//VOID CALLBACK TimerProc(HWND hWnd, UINT uMsg, UINT_PTR idEvent, DWORD dwTime)
+//{
+//    t = time(NULL);
+//    tm* timerinfo = localtime(&t);
+//    lstrcpy(_timebuf, _tctime(&t));
+//    //wsprintf(_timebuf, TEXT("%d"), timerinfo);
+//    /*TextOut(hdc, 0, 0, _timebuf, wcslen(_timebuf));*/
+//}
 
-
-
+void CALLBACK TimerProc(HWND hWnd, UINT msg, UINT idTimer, DWORD dwTime)
+{
+    MessageBeep(-1);
+    return;
+}
 //
 //  ФУНКЦИЯ: MyRegisterClass()
 //
@@ -88,15 +88,15 @@ ATOM MyRegisterClass(HINSTANCE hInstance)
 
     wcex.cbSize = sizeof(WNDCLASSEX);
 
-    wcex.style          = CS_HREDRAW | CS_VREDRAW | CS_DBLCLKS;
+    wcex.style          = CS_HREDRAW | CS_VREDRAW;
     wcex.lpfnWndProc    = WndProc;
     wcex.cbClsExtra     = 0;
     wcex.cbWndExtra     = 0;
     wcex.hInstance      = hInstance;
-    wcex.hIcon          = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_FORMCAPP));
+    wcex.hIcon          = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_WATCH));
     wcex.hCursor        = LoadCursor(nullptr, IDC_ARROW);
     wcex.hbrBackground  = (HBRUSH)(COLOR_WINDOW+1);
-    wcex.lpszMenuName   = MAKEINTRESOURCEW(IDC_FORMCAPP);
+    wcex.lpszMenuName   = MAKEINTRESOURCEW(IDC_WATCH);
     wcex.lpszClassName  = szWindowClass;
     wcex.hIconSm        = LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_SMALL));
 
@@ -117,8 +117,8 @@ BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
 {
    hInst = hInstance; // Сохранить маркер экземпляра в глобальной переменной
 
-   HWND hWnd = CreateWindowW(szWindowClass, szTitle, WS_OVERLAPPEDWINDOW,
-       x, y, width, height, nullptr, nullptr, hInstance, nullptr);
+   HWND hWnd = CreateWindowW(szWindowClass, szTitle, WS_OVERLAPPEDWINDOW | WS_THICKFRAME,
+      CW_USEDEFAULT, 0, 300, 300, nullptr, nullptr, hInstance, nullptr);
 
    if (!hWnd)
    {
@@ -130,7 +130,15 @@ BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
 
    return TRUE;
 }
-
+//void showClock(HDC hdc)
+//{
+//    SetTimer(hWnd, 1, 1000, NULL);
+//    t = time(NULL);
+//    tm* timerinfo = localtime(&t);
+//    lstrcpy(_timebuf, _tctime(&t));
+//    //wsprintf(_timebuf, TEXT("%d"), timerinfo);
+//    TextOut(hdc, 0, 0, _timebuf, wcslen(_timebuf));
+//}
 //
 //  ФУНКЦИЯ: WndProc(HWND, UINT, WPARAM, LPARAM)
 //
@@ -143,7 +151,6 @@ BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
 //
 LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 {
-    WCHAR str[50];
     switch (message)
     {
     case WM_COMMAND:
@@ -163,110 +170,36 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
             }
         }
         break;
-    case WM_SIZE:
-        cx = LOWORD(lParam);
-        cy = HIWORD(lParam);
-    case WM_LBUTTONDOWN:
-        l++;
-        SetTitle();
-        SetWindowText(hWnd, szTitle);
-        Wx = LOWORD(lParam);
-        Wy = HIWORD(lParam);
-        if ((Wx < 10 || Wx > cx-10) || (Wy < 10 ||Wy > cy-10))
-        {
-            wsprintf(szTitle, TEXT("%s"), L"Щелчок снаружи прямоугольника");
-            SetWindowText(hWnd, szTitle);
-        }
-        else if ((Wx < cx -10 || Wx >10)|| (Wy < cy -10 || Wy >10))
-        {
-            wsprintf(szTitle, TEXT("%s"), L"Щелчок внутри прямоугольника");
-            SetWindowText(hWnd, szTitle);
-        }
-       
-        break;
+    case WM_CREATE:
+        SetTimer(hWnd, CLOCK_TIMER, 1000, NULL);
+        nBeepTimerID = SetTimer(hWnd, 0, 1000, (TIMERPROC)TimerProc);
 
-    case WM_RBUTTONDOWN:
-        r++;
-        SetTitle();
-        SetWindowText(hWnd, szTitle);
-        width = cx;
-        height = cy;
-        wsprintf(szTitle, TEXT("w=%d h=%d"), width, height);
-        SetWindowText(hWnd, szTitle);
+    case WM_TIMER:
+        InvalidateRect(hWnd, NULL, FALSE);
+        return 0;        
         
-        break;
-    
-    case WM_MBUTTONDOWN:
-        m++;
-        SetTitle();
-        SetWindowText(hWnd, szTitle);
-        break;
-
-    case WM_LBUTTONDBLCLK:
-        ld++;
-        SetTitle();
-        SetWindowText(hWnd, szTitle);
-        break;
-
-    case WM_RBUTTONDBLCLK:
-        rd++;
-        SetTitle();
-        SetWindowText(hWnd, szTitle);
-        break;
-    case WM_MBUTTONDBLCLK:
-        md++;
-        SetTitle();
-        SetWindowText(hWnd, szTitle);
-        break;
-    case WM_MOUSEMOVE:
-      /*  wsprintf(szTitle, TEXT("X=%d Y=%d"), LOWORD(lParam), HIWORD(lParam));
-        SetWindowText(hWnd, szTitle);*/
-        break;
-   /* case WM_CHAR:
-        wsprintf(str, TEXT("Нажата клавиша %c"),(WCHAR)wParam);
-        MessageBox(0, str, TEXT("WM_CHAR"), MB_OK | MB_ICONINFORMATION);
-        break;*/
-    case WM_KEYDOWN:
-       
-        if ((WCHAR)wParam == VK_RETURN)
-        {
-            MoveWindow(hWnd, 0, 0, 300, 300, TRUE);
-            WinX = GetSystemMetrics(SM_CXSCREEN);
-            WinY = GetSystemMetrics(SM_CYSCREEN);
-            /*wsprintf(szTitle, TEXT("X=%d Y=%d"), WinX, WinY);
-            SetWindowText(hWnd, szTitle);*/
-            for (int i = 0; i < WinX; i++)
-            {
-                for (int j = 0; j < WinY; j++)
-                {
-                    MoveWindow(hWnd, i, j, 300, 300, TRUE);
-                }
-            }
-            //wsprintf(str, TEXT("Нажата клавиша Enter %c"), (WCHAR)wParam); //Формирование сообщения
-            //MessageBox(0, str, TEXT("WM_CHAR"), MB_OK | MB_ICONINFORMATION); //Принятие сообщение в MessageBox и вывод MessageBox 
-        }
-        else if ((WCHAR)wParam == VK_ESCAPE)
-        {
-            BringWindowToTop(hWnd);
-            break;
-        }
-            
-
-       /* else if (wParam == VK_ESCAPE)
-        {
-            MoveWindow(hWnd, 0, 0, 300, 300, TRUE);
-            break;
-        }*/
-
     case WM_PAINT:
         {
-            PAINTSTRUCT ps;
-            HDC hdc = BeginPaint(hWnd, &ps);
+        WCHAR szBuf[80];
+        int nBufSize;
+        PAINTSTRUCT ps;
+        time_t t;
+        struct tm* ltime;
+        RECT rc;
+        HDC hdc = BeginPaint(hWnd, &ps);
             // TODO: Добавьте сюда любой код прорисовки, использующий HDC...
-            EndPaint(hWnd, &ps);
+        time(&t);
+        ltime = localtime(&t);
+        nBufSize = wsprintf(szBuf, L"%02d:%02d:%02d", ltime->tm_hour, ltime->tm_min, ltime->tm_sec);
+        SelectObject(hdc, GetStockObject(DEVICE_DEFAULT_FONT));//SYSTEM_FIXED_FONT
+        GetClientRect(hWnd, &rc);
+        DrawText(hdc, szBuf, nBufSize, &rc, DT_CENTER | DT_VCENTER | DT_NOCLIP | DT_SINGLELINE);
+        EndPaint(hWnd, &ps);
         }
         break;
     case WM_DESTROY:
+        KillTimer(hWnd, CLOCK_TIMER);
+        KillTimer(hWnd, nBeepTimerID);
         PostQuitMessage(0);
         break;
     default:
